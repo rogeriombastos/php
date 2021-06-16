ae7284ae7ac8e9f46c4bad5e960f69d06ccf10d7
Rogério Martins Bastos <rogeriombastos@gmail.com>
2021-06-16 09:56:49 -0300

    Resolvi o conflito

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
 <?php
 
-<<<<<<< HEAD
 echo "Meu primeiro código PHP!  Olá mundo 456";
 
-echo "Olá Rogério";
-=======
-echo "Meu primeiro código PHP!  Olá mundo 123";
-
-echo "Olá hcode";
->>>>>>> d680e99c2b3cd027d2aa54d412361bad556fed92
+echo "Olá Rogério e Hcode";
 
 ?>
